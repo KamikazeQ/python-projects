80b6afbbe8958d4fdfda2185577299a34e295f96
Quinton Gordon <quinton.gordon@gmail.com>
2017-09-20 21:41:19 -0400

    Ready for submittal
    version of program that I want to submit

--- league_builder.py
+++ league_builder.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     Dragons = []
     Raptors = []
 
+    # Used for identifying team
     turn = 1
 
     # Opens and reads the csv.  Places the experienced and inexperienced into two different lists.
@@ -30,7 +31,7 @@ if __name__ == "__main__":
                 del row[1]
                 not_experienced.append(row)
 
-    # Assignes experienced kids among the three teams
+    # Assigns experienced kids among the three teams
     while len(experienced):
         if turn == 1:
             Sharks.append(experienced.pop())
@@ -58,13 +59,27 @@ if __name__ == "__main__":
     Create a text file named teams.txt that includes the name of a team, followed by the players on that team. List 
     all three teams and their players.
     '''
+    '''
+    TO EXCEED EXPECTATIONS:
+    In the list of teams, include the team name on one line, followed by a separate line for each player. Include the 
+    player's name, whether the player has experience playing soccer, and the player's guardian names. Separate each 
+    bit of player information by a comma.
+    '''
 
+    # Create the main output file
     file = open("teams.txt", "w")
     file.write("Sharks\n")
+    # Used to iterate through team list
     count = 0
+    # writes the info and creates the letters
     for item in Sharks:
         file.write("Player: {}, is Experienced: {},"
                    " name of guardians: {}\n".format(Sharks[count][0], Sharks[count][1], Sharks[count][2]))
+        # Create letter
+        letter = open(str.lower(Sharks[count][0]).replace(" ", "_") + ".txt", "w")
+        letter.write("Dear {},\n\n"
+                     "Your child, {}, has been assigned to the Sharks team.\nThe first practice is scheduled for "
+                     "October 1st at 5PM.".format(Sharks[count][2], Sharks[count][0]))
         count += 1
     file.write("\n")
     file.write("Dragons\n")
@@ -72,6 +87,11 @@ if __name__ == "__main__":
     for item in Dragons:
         file.write("Player: {}, is Experienced: {},"
                    " name of guardians: {}\n".format(Dragons[count][0], Dragons[count][1], Dragons[count][2]))
+        # Create letter
+        letter = open(str.lower(Dragons[count][0]).replace(" ", "_") + ".txt", "w")
+        letter.write("Dear {},\n\n"
+                     "Your child, {}, has been assigned to the Dragons team.\nThe first practice is scheduled for "
+                     "October 1st at 5PM.".format(Dragons[count][2], Dragons[count][0]))
         count += 1
     file.write("\n")
     file.write("Raptors\n")
@@ -79,11 +99,11 @@ if __name__ == "__main__":
     for item in Raptors:
         file.write("Player: {}, is Experienced: {},"
                    " name of guardians: {}\n".format(Raptors[count][0], Raptors[count][1], Raptors[count][2]))
+        # Create letter
+        letter = open(str.lower(Raptors[count][0]).replace(" ", "_") + ".txt", "w")
+        letter.write("Dear {},\n\n"
+                     "Your child, {}, has been assigned to the Raptors team.\nThe first practice is scheduled for "
+                     "October 1st at 5PM.".format(Raptors[count][2], Raptors[count][0]))
         count += 1
     file.write("\n")
 
-    '''
-    In the list of teams, include the team name on one line, followed by a separate line for each player. Include the 
-    player's name, whether the player has experience playing soccer, and the player's guardian names. Separate each 
-    bit of player information by a comma.
-    '''
